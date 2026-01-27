3dc23ee8ab721debea771180f428666a2b7f7155
HeinoPorfolio <meheino@sbcglobal.net>
2026-01-27 18:18:23 -0500

    refactroring of the FileSaver and ConsoleUI into classes

---
+++ BusShuttleProject/BusShuttle/FileSaver.cs
@@ -0,0 +1,20 @@
+
+namespace BusShuttle;
+public class FileSaver
+{
+   string fileName;
+
+    public FileSaver(string fileName)
+    {
+        this.fileName = fileName;
+        // Create the file
+        File.Create(this.fileName).Close();
+
+    }
+
+    public void AppendLine(string line)
+    {
+        File.AppendAllText(this.fileName, line+Environment.NewLine);
+    }
+
+}
--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,8 +1,5 @@
 ﻿namespace BusShuttle;
 
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-
 class Program
 {
     static void Main(string[] args)
@@ -12,58 +9,4 @@ class Program
     }
 }
 
-public class ConsoleUI
-{
-    FileSaver fileSaver;
 
-    public ConsoleUI()
-    {
-       fileSaver = new FileSaver("passenger-data.txt"); 
-    }
-
-    public void Show()
-    {
-        string mode= AskForInput("Please select mode(driver OR manager): ");
-
-        if (mode =="driver"){
-
-            string command;
-
-            do{
-                
-                string stopName = AskForInput("Enter stop name: ");
-                int boarded = int.Parse(AskForInput("Enter numbered of boarded passengers: "));
-                // Append data to a file
-                fileSaver.AppendLine(stopName+":"+boarded);
-                command = AskForInput("Enter command (end or continue): ");
-
-            }while(command != "end");
-        }
-    }
-
-    public static string AskForInput(string message)
-    {
-        Console.Write(message);
-        return Console.ReadLine();
-
-    }
-}
-
-public class FileSaver
-{
-   string fileName;
-
-    public FileSaver(string fileName)
-    {
-        this.fileName = fileName;
-        // Create the file
-        File.Create(this.fileName).Close();
-
-    }
-
-    public void AppendLine(string line)
-    {
-        File.AppendAllText(this.fileName, line+Environment.NewLine);
-    }
-
-}
