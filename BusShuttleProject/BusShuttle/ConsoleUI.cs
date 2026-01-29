da9dcc335a4e25232b65b7735ab5b5e76b58de92
HeinoPorfolio <meheino@sbcglobal.net>
2026-01-29 02:59:17 -0500

    initial ui with spectre

--- BusShuttleProject/BusShuttle/ConsoleUI.cs
+++ BusShuttleProject/BusShuttle/ConsoleUI.cs
@@ -6,34 +6,70 @@ public class ConsoleUI
 {
     FileSaver fileSaver;
 
+    // List of loops
+    List<Loop> loops;
+
+    // List of stops
+    List<Stop> stops;
+
     public ConsoleUI()
     {
-       fileSaver = new FileSaver("passenger-data.txt"); 
+        fileSaver = new FileSaver("passenger-data.txt");
+        // Create the loops
+        loops = new List<Loop>();
+        // Add loops to a list
+        loops.Add(new Loop("Red"));
+        loops.Add(new Loop("Green"));
+        loops.Add(new Loop("Blue"));
+
+        // Add stops to a list
+        stops = new List<Stop>();
+        stops.Add(new Stop("Music"));  
+        stops.Add(new Stop("Towers"));
+        stops.Add(new Stop("Oakwood"));
+        stops.Add(new Stop("Anthony"));
+        stops.Add(new Stop("Letterman"));
+
+        // Assign them to the first loop
+        loops[0].Stops.Add(stops[0]);
+        loops[0].Stops.Add(stops[1]);
+        loops[0].Stops.Add(stops[2]);
+        loops[0].Stops.Add(stops[3]);
+        loops[0].Stops.Add(stops[4]);
+
     }
     public void Show()
     {
         
-        // Spectre.Console Addition
         var mode = AnsiConsole.Prompt(new SelectionPrompt<string>()
-        .Title("Please select mode").AddChoices(new[]
+            .Title("Please select mode").AddChoices(new[]
         {
             "driver","manager"
         }));
 
         if (mode =="driver"){
 
+            Loop selectedLoop = AnsiConsole.Prompt(new SelectionPrompt<Loop>()
+                .Title("Select a loop").AddChoices(loops));
+            Console.WriteLine("Your selected loop: " + selectedLoop.Name); 
+            
             string command;
 
             do{
-                
-                string stopName = AskForInput("Enter stop name: ");
+            
+                Stop selectedStop = AnsiConsole.Prompt(new SelectionPrompt<Stop>()
+                    .Title("Select a stop")
+                    .AddChoices(selectedLoop.Stops));
+                Console.WriteLine("Your selected stop: " + selectedStop.Name); 
+
                 int boarded = int.Parse(AskForInput("Enter numbered of boarded passengers: "));
 
                 // Append data to a file
-                fileSaver.AppendLine(stopName+":"+boarded);
+                fileSaver.AppendLine(selectedStop.Name+":"+boarded);
                 
                 command = AnsiConsole.Prompt(new SelectionPrompt<string>()
-                    .Title("What's next?").AddChoices(new[]
+                    .Title("What's next?")
+                    .AddChoices(new[]
                     {
                         "continue","end"
                     }));
---
+++ BusShuttleProject/BusShuttle/Domain.cs
@@ -0,0 +1,70 @@
+using System.Dynamic;
+
+namespace BusShuttle;
+
+// Class for the Stop
+public class Stop
+{
+    public string Name {get; }
+
+    public Stop(string name)
+    {
+        this.Name = name;
+    }
+    public override string ToString()
+    {
+        return this.Name;
+    }
+} //end stop
+
+// Class for Driver
+public class Driver
+{
+    public string Name {get;}
+
+    public Driver(string name)
+    {
+        this.Name = name;
+    }
+} // end Driver
+
+public class Loop
+{
+    public string Name;
+
+    // Contains an loop of stops
+    // Will be contained in a list
+    public List<Stop> Stops {get; }
+
+    public Loop(string name)
+    {
+        this.Name = name;
+        this.Stops = new List<Stop>();
+    }
+
+    public override string ToString()
+    {
+        return this.Name;
+    }
+
+} // end Loop
+
+public class PassengerData
+{
+    public int Boarded {get;}
+    public Stop Stop {get;}
+    public Loop Loop {get;}
+    public Driver Driver {get;}
+
+    public PassengerData(int boarded, Stop stop, Loop loop, Driver driver)
+    {
+        this.Boarded = boarded;
+        this.Stop = stop;
+        this.Loop = loop;
+        this.Driver = driver;
+    }
+
+
+
+
+} // end PassengerData
